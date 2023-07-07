bf151baf4151c2803604c2ed53b4e9444fd28fb2
Ashish Dubey <ashishdubey1702@gmail.com>
2023-07-07 16:11:29 +0530

    updated

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,10 +1,26 @@
 import {React} from "react";
 import "./Home.scss"
 
+const Card=({img})=>{ 
+     return<img className="card" src={img} alt="cover"/>
+}
+
+const Row=({title})=>(
+
+    <div className="row">
+
+         <h2>{title}</h2>
+
+         <Card img={"https://th-i.thgim.com/public/incoming/nt52c4/article66177563.ece/alternates/FREE_1200/IMG_Kantara_0_x_2_1_02AFBGSL.jpg"} />
+
+    </div>
+)
+
 const Home=() =>{
     return(
         <section className="home">
             <div className="banner"></div>
+            <Row title={"Popular on Netflix"}/>
         </section>
     )
 }
