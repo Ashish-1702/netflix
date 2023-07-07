f71ec2956d1da3f1bb956a604a1fb3e983a311f7
Ashish Dubey <ashishdubey1702@gmail.com>
2023-07-07 16:22:34 +0530

    update

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -10,9 +10,12 @@ const Row=({title})=>(
     <div className="row">
 
          <h2>{title}</h2>
-
-         <Card img={"https://th-i.thgim.com/public/incoming/nt52c4/article66177563.ece/alternates/FREE_1200/IMG_Kantara_0_x_2_1_02AFBGSL.jpg"} />
-
+         <div>
+           <Card img={"https://th-i.thgim.com/public/incoming/nt52c4/article66177563.ece/alternates/FREE_1200/IMG_Kantara_0_x_2_1_02AFBGSL.jpg"} />
+           <Card img={"https://th-i.thgim.com/public/incoming/nt52c4/article66177563.ece/alternates/FREE_1200/IMG_Kantara_0_x_2_1_02AFBGSL.jpg"} />
+           <Card img={"https://th-i.thgim.com/public/incoming/nt52c4/article66177563.ece/alternates/FREE_1200/IMG_Kantara_0_x_2_1_02AFBGSL.jpg"} />
+           <Card img={"https://th-i.thgim.com/public/incoming/nt52c4/article66177563.ece/alternates/FREE_1200/IMG_Kantara_0_x_2_1_02AFBGSL.jpg"} />
+         </div>
     </div>
 )
 
